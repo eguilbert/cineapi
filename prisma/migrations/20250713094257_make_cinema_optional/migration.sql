1dc041011789db0f87b6b76a703e3de8e4d8db17
eguilbert <eguilbert@jalons.com>
2025-07-16 04:53:54 +0200

    💿 Recréation propre du schéma avec username

--- prisma/migrations/20250713094257_make_cinema_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."UserProfile" DROP CONSTRAINT "UserProfile_cinemaId_fkey";
-
--- AlterTable
-ALTER TABLE "public"."UserProfile" ALTER COLUMN "cinemaId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "public"."UserProfile" ADD CONSTRAINT "UserProfile_cinemaId_fkey" FOREIGN KEY ("cinemaId") REFERENCES "public"."Cinema"("id") ON DELETE SET NULL ON UPDATE CASCADE;
