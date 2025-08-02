17b390f7ec445b76efb665e6b7c4a6735c73f714
eguilbert <eguilbert@jalons.com>
2025-08-02 19:37:37 +0200

    udpate auth

--- routes/auth.js
+++ routes/auth.js
@@ -53,7 +53,7 @@ router.post("/register", async (req, res) => {
         stack: err.stack,
       });
     }
-    console.log("✅ Utilisateur créé:", user);
+    console.log("✅ Utilisateur créé:", result);
 
     /*     const session = await lucia.createSession(user.id);
     console.log("🔑 Session créée:", session.id); */
