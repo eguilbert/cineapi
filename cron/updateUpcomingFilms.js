1178f5691fdd26f4a99a7848a95a7ccc2243cc0c
eguilbert <eguilbert@jalons.com>
2025-09-02 22:48:18 +0200

    test route 5

--- cron/updateUpcomingFilms.js
+++ cron/updateUpcomingFilms.js
@@ -21,6 +21,9 @@ export async function updateUpcomingFilms() {
         gte: today.toDate(),
         lte: inThreeMonths.toDate(),
       },
+      select: {
+        tmdbId: true,
+      },
     },
   });
 
