623a36ae5bb75caedfa9ae3eb067a8085ede283a
eguilbert <eguilbert@jalons.com>
2025-09-02 22:04:19 +0200

    test route 2

--- routes/cron.js
+++ routes/cron.js
@@ -7,9 +7,15 @@ const router = Router();
  * GET /api/cron
  */
 router.get("/update-upcoming", async (req, res) => {
-  /* await updateUpcomingFilms(); */
-  console.log("Route cron appelée !");
-  res.json({ status: "OK" });
+  try {
+    console.log("CRON: Début updateUpcomingFilms");
+    await updateUpcomingFilms();
+    console.log("CRON: Fin updateUpcomingFilms");
+    res.json({ status: "OK" });
+  } catch (err) {
+    console.error("CRON ERROR:", err);
+    res.status(500).json({ error: "Échec de mise à jour des films" });
+  }
 });
 
 export default router;
